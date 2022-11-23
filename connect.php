15154a8d6b75d85b98e1272f309571c391163ddb
johnoliveira06 <jhonoliver567@gmail.com>
2022-11-23 17:39:49 -0300

    Atualização do cadastro

--- connect.php
+++ connect.php
@@ -1,26 +1,19 @@
 <?php
-  
-$server = "localhost";
-$username = "root";
-$password = "";
-$dbname = "beautyface";
- 
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'beautyface';
+$port = '3306';
 
-try {
-    $connection = new PDO("mysql:host=$server;dbname=" . $dbname, $username, $password);
+$connection = mysqli_connect($server, $username, $password, $database, $port);
 
-    // echo "CONECTADO!!";
-} catch (PDOException $erro) {
-    echo "Erro: Conexão com banco de dados não realizado com sucesso. Erro gerando " . $erro->getMessage();
+if ($connection->connect_errno) {
+    echo 'Falha ao conectar ao MySQL: (' .
+        $connection->connect_errno .
+        ') ' .
+        $connection->connect_errno;
 }
-// $connection = mysqli_connect($server, $username, $password, $database);
-  
-// if ($connection->connect_errno) {
-//     echo 'Falha ao conectar ao MySQL: (' .
-//         $connection->connect_errno .
-//         ') ' .
-//         $connection->connect_errno;
-// }
 
-// mysqli_set_charset($connection, 'utf8');
+mysqli_set_charset($connection, 'utf8');
+
 ?>
--- insert.php
+++ insert.php
@@ -1,49 +1,42 @@
 <?php
 include_once ('connect.php');
 
-$dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 $cidade = $_POST['cidade'];
 
-// echo "valores:";
-// print_r($_POST['nome']);
-
-$sql = "INSERT INTO users(nome,senha,email,cidade) VALUES ('$nome', '$senha', '$email', '$cidade')";
-
-if ($result = mysqli_query($connection, $sql)) {
-    echo "Espaço cadastrado com sucesso.
-    <a href='index.php'>Voltar</a>
-  ";
-} else {
-    echo "Erro ao cadastrar espaço.
-    <a href='index.php'>Voltar</a>
-  ";
+if(empty($nome)){
+  echo "<script>alert('Todos os dados devem ser preenchidos!')
+  window.location.href = 'cadastro.php'
+  </script>";
 }
-
-
-// if(empty($dados['nome'])){
-//   echo "<script>alert('Nome não pode ser vazio!')
-//   window.location.href = 'cadastro.php'
-//   </script>";
-// }
-// elseif(empty($dados['email'])){
-//   echo"<script> alert('E-mail não pode ser vazio!');
-//   window.location.href='cadastro.php';
-//   </script>";
-// }
-// elseif(empty($dados['senha'])){
-//   echo"<script> alert('Senha não pode ser vazio!');
-//   window.location.href='cadastro.php';
-//   </script>";
-// }
-// elseif(empty($dados['cidade'])){
-//   echo"<script> alert('Cidade não pode ser vazio!');
-//   window.location.href='cadastro.php';
-//   </script>";
-// }
+if(empty($email)){
+  echo"<script> alert('Todos os dados devem ser preenchidos!');
+  window.location.href='cadastro.php';
+  </script>";
+}
+elseif(empty($senha)){
+  echo"<script> alert('Todos os dados devem ser preenchidos!');
+  window.location.href='cadastro.php';
+  </script>";
+}
+elseif(empty($cidade)){
+  echo"<script> alert('Todos os dados devem ser preenchidos!');
+  window.location.href='cadastro.php';
+  </script>";
+}else {
+  $sql = "INSERT INTO users(nome,senha,email,cidade) VALUES ('$nome', '$senha', '$email', '$cidade')";
+  if ($result = mysqli_query($connection, $sql)) {
+    echo"<script> alert('Usuário cadastrado com sucesso!');
+    window.location.href='cadastro.php';
+    </script>";
+  } else {
+    echo"<script> alert('Não foi possivél realizar o cadastro');
+    window.location.href='cadastro.php';
+    </script>";
+  }
+}
 // else{
 //   $query_consulta = "SELECT id FROM users WHERE email=:email LIMIT 1";
 //   $result_consulta = $conn->prepare($query_consulta);
@@ -56,32 +49,6 @@ if ($result = mysqli_query($connection, $sql)) {
 //       </script>";
 //   }
 
-  // $sql = "INSERT INTO users(nome,email,senha,cidade) VALUES (:nome, :email, :senha, :cidade)";
 
-  // $cadastro = $connection->prepare($sql);
-
-  // $cadastro->bindParam(':nome', $dados['nome'], PDO::PARAM_STR);
-  // $cadastro->bindParam(':email', $dados['email'], PDO::PARAM_STR);
-  // $cadastro->bindParam(':senha', $dados['senha'], PDO::PARAM_STR);
-  // $cadastro->bindParam(':senha', $dados['senha'], PDO::PARAM_STR);
-  // $cadastro->bindParam(':cidade', $dados['cidade'], PDO::PARAM_STR);
-
-  // $cadastro->execute();
-
-  // if($cadastro->rowCount()){
-  //   echo"<script> alert('Usuário cadastrado com sucesso!');
-  //   window.location.href='cadastro.php';
-  //   </script>";
-  //   }else{
-  //       echo"<script> alert('Não foi possivél realizar o cadastro');
-  //   window.location.href='cadastro.php';
-  //   </script>";
-  //   }
 
 ?>
-
-<!-- <html>
-<head>
-
-
-// ?> -->
