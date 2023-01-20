90fa18a2063370d187bb026cc0e3c60bbba65399
johnoliveira06 <jhonoliver567@gmail.com>
2023-01-20 18:02:40 -0300

    Correção nos arquivos sql

--- documents/products.sql
+++ documents/products.sql
@@ -1,17 +1,17 @@
 CREATE TABLE IF NOT EXISTS produtos (
   id INT(11) NOT NULL AUTO_INCREMENT,
-  nome VARCHAR(11) NOT NULL,
+  nome VARCHAR(255) NOT NULL,
   preco VARCHAR(255) NOT NULL,
   imagem VARCHAR(255) NULL DEFAULT NULL,
   PRIMARY KEY (id))
 ENGINE = InnoDB;
 
-INSERT INTO `produtos`(`id`, `nome`, `preco`, `imagem`) VALUES (1,'Sabonete','20, 00','');
-INSERT INTO `produtos`(`id`, `nome`, `preco`, `imagem`) VALUES (2,'Shampoo','15, 00','');
-INSERT INTO `produtos`(`id`, `nome`, `preco`, `imagem`) VALUES (3,'Kit Shampoo','30, 00','');
-INSERT INTO `produtos`(`id`, `nome`, `preco`, `imagem`) VALUES (4,'Shampoo','20, 00','');
-INSERT INTO `produtos`(`id`, `nome`, `preco`, `imagem`) VALUES (5,'Creme Facial','25, 00','');
-INSERT INTO `produtos`(`id`, `nome`, `preco`, `imagem`) VALUES (6,'Perfume','80, 00','');
-INSERT INTO `produtos`(`id`, `nome`, `preco`, `imagem`) VALUES (7,'Protetor Solar','35, 00','');
-INSERT INTO `produtos`(`id`, `nome`, `preco`, `imagem`) VALUES (8,'Kit Creme','70, 00','');
-INSERT INTO `produtos`(`id`, `nome`, `preco`, `imagem`) VALUES (9,'Anti Rugas','40, 00','');
+INSERT INTO `produtos`(`id`, `nome`, `preco`, `imagem`) VALUES (NULL, 'Sabonete','20, 00','');
+INSERT INTO `produtos`(`id`, `nome`, `preco`, `imagem`) VALUES (NULL,'Shampoo','15, 00','');
+INSERT INTO `produtos`(`id`, `nome`, `preco`, `imagem`) VALUES (NULL,'Kit Shampoo','30, 00','');
+INSERT INTO `produtos`(`id`, `nome`, `preco`, `imagem`) VALUES (NULL,'Shampoo','20, 00','');
+INSERT INTO `produtos`(`id`, `nome`, `preco`, `imagem`) VALUES (NULL,'Creme Facial','25, 00','');
+INSERT INTO `produtos`(`id`, `nome`, `preco`, `imagem`) VALUES (NULL,'Perfume','80, 00','');
+INSERT INTO `produtos`(`id`, `nome`, `preco`, `imagem`) VALUES (NULL,'Protetor Solar','35, 00','');
+INSERT INTO `produtos`(`id`, `nome`, `preco`, `imagem`) VALUES (NULL,'Kit Creme','70, 00','');
+INSERT INTO `produtos`(`id`, `nome`, `preco`, `imagem`) VALUES (NULL,'Anti Rugas','40, 00','');
--- documents/users.sql
+++ documents/users.sql
@@ -12,5 +12,5 @@ CREATE TABLE IF NOT EXISTS `beautyface`.`users` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-INSERT INTO `users`(`id`, `nome`, `email`, `senha`, `cidade`) VALUES (1,'john','john@gmail.com','12345','santarém');
-INSERT INTO `users`(`id`, `nome`, `email`, `senha`, `cidade`) VALUES (2,'nome','nome@gmail.com','123456','cidade');
+INSERT INTO `users`(`id`, `nome`, `email`, `senha`, `cidade`) VALUES (NULL,'john','john@gmail.com','12345','santarém');
+INSERT INTO `users`(`id`, `nome`, `email`, `senha`, `cidade`) VALUES (NULL,'nome','nome@gmail.com','123456','cidade');
